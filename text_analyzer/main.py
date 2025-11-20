ea92c8ebc471483fb974484ea5fd9d1dcf77896b
VishvaThirumalai <vishva2730@gmail.com>
2025-11-20 23:11:00 +0530

    Update main.py

--- text_analyzer/main.py
+++ text_analyzer/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://text-analyzer-frontend-v0.onrender.com"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://text-analyzer-frontend-v0.onrender.com","https://text-analyser-frontend-v1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
